89db76dd6a9a1f844bf0d13431911dc25904d3d2
Stan Wilmsen <413970@student.fontys.nl>
2020-06-16 22:10:02 +0200

    Added renewed files from Mona Lisa POC

---
+++ script.js
@@ -0,0 +1,38 @@
+let c = document.createElement("canvas");
+ctx = c.getContext('2d');
+let img1 = new Image();
+let sample_size = 400;
+
+function clicker() {
+
+    w = img1.width;
+    h = img1.height;
+
+    c.width = w;
+    c.height = h;
+    ctx.drawImage(img1, 0, 0);
+
+    var pixelArr = ctx.getImageData(0, 0, w, h).data;
+
+
+    for (let y = 0; y < h; y += sample_size) {
+        for (let x = 0; x < w; x += sample_size) {
+            let p = (x + (y * w)) * 4;
+            ctx.fillStyle = "rgba(" + pixelArr[p] + "," + pixelArr[p + 1] + "," + pixelArr[p + 2] + "," + pixelArr[p + 3] + ")";
+            ctx.fillRect(x, y, sample_size, sample_size);
+        }
+    }
+
+    let img2 = new Image();
+    img2.src = c.toDataURL("image/jpeg");
+    img2.width = 400;
+
+    if (document.body === img2) {
+        img2.parentNode.removeChild(img2);
+    }
+    else {
+        document.body.appendChild(img2);
+    }
+};
+
+img1.src = document.getElementById("image1").src;
