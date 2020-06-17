9dddf6d9e894827d2aca91a3d5edac9009b84e0a
Stan Wilmsen <413970@student.fontys.nl>
2020-06-17 12:56:44 +0200

    Changed nodes of elements to change when clicked

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
-let c = document.createElement("canvas");
-ctx = c.getContext('2d');
-let img1 = new Image();
-let sample_size = 400;
+let canvas = document.createElement("canvas");
+ctx = canvas.getContext('2d');
+let image1 = new Image();
+let pixelsize = 400;
+
+image1.src = document.getElementById("image1").src;
 
 function clicker() {
+    if (pixelsize == 0) {
+        pixelsize = 400;
+    }
+    width = image1.width;
+    height = image1.height;
 
-    w = img1.width;
-    h = img1.height;
+    canvas.width = width;
+    canvas.height = height;
+    ctx.drawImage(image1, 0, 0);
 
-    c.width = w;
-    c.height = h;
-    ctx.drawImage(img1, 0, 0);
-
-    var pixelArr = ctx.getImageData(0, 0, w, h).data;
+    var rgb = ctx.getImageData(0, 0, width, height).data;
 
 
-    for (let y = 0; y < h; y += sample_size) {
-        for (let x = 0; x < w; x += sample_size) {
-            let p = (x + (y * w)) * 4;
-            ctx.fillStyle = "rgba(" + pixelArr[p] + "," + pixelArr[p + 1] + "," + pixelArr[p + 2] + "," + pixelArr[p + 3] + ")";
-            ctx.fillRect(x, y, sample_size, sample_size);
+    for (let i = 0; i < height; i += pixelsize) { //hoevaak de loop moet 'mengen'
+        for (let x = 0; x < width; x += pixelsize) {
+            let pixels = (x + (i * width)) * 4;     //positie van de pixelarray
+            ctx.fillStyle = "rgba(" + rgb[pixels] + "," + rgb[pixels + 1] + "," + rgb[pixels + 2] + "," + rgb[pixels + 3] + ")"; //rgba versie van kleur opslaan
+            ctx.fillRect(x, i, pixelsize, pixelsize);
         }
     }
 
-    let img2 = new Image();
-    img2.src = c.toDataURL("image/jpeg");
-    img2.width = 400;
-
-    if (document.body === img2) {
-        img2.parentNode.removeChild(img2);
+    let image2 = new Image(); //tweede image object voor de pixelated versie
+    image2.setAttribute("id", "image2");
+    image2.src = canvas.toDataURL("image/jpeg");
+    image2.width = 600;
+    if (document.body.contains(document.getElementById("image2"))) { //als het object al bestaat moet deze weggehaald worden
+        document.getElementById("image2").remove();
     }
-    else {
-        document.body.appendChild(img2);
+    document.body.appendChild(image2);
+    if (pixelsize > 50) {
+        pixelsize -= 50;
     }
+    else if (pixelsize > 0 || pixelsize < 50) {
+        pixelsize -= 2;
+    }
+    console.log(pixelsize);
 };
-
-img1.src = document.getElementById("image1").src;
